642cfbb95f11c5bc3dda9f098995929dac6228fa
Zhang Wei <zhang.wei@espressif.com>
2023-02-03 16:01:53 +0800

    Supress format warnings for IDF v5.0 and above.

--- examples/subscribe_publish/main/subscribe_publish_sample.c
+++ examples/subscribe_publish/main/subscribe_publish_sample.c
@@ -161,7 +161,7 @@ void disconnectCallbackHandler(AWS_IoT_Client *pClient, void *data) {
 void aws_iot_task(void *param) {
     char cPayload[100];
 
-    int32_t i = 0;
+    int i = 0;
 
     IoT_Error_t rc = FAILURE;
 
